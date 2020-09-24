f00a97cecc527c337a8ff927d6a40bfa5f490909
Thiago Valentim <thiago_valentim1@hotmail.com>
2020-09-24 18:52:42 -0300

    Casos de teste fechados: #1, #2

--- src/main/java/br/ufes/model/Cliente.java
+++ src/main/java/br/ufes/model/Cliente.java
@@ -2,16 +2,30 @@ package br.ufes.model;
 
 public final class Cliente {
 
-    private final String nome;
-    private final String CNPJOuCPF;
+    private String nome;
+    private String CNPJOuCPF;
 
     public Cliente(String nome, String codigo) {
-        this.nome = nome;
-        this.CNPJOuCPF = codigo;
+        setNome(nome);
+        setCNPJOuCPF(codigo);
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome == null) {
+            throw new IllegalArgumentException("nome não deve ser null!");
+        }
+        this.nome = nome;
+    }
+
+    public void setCNPJOuCPF(String codigo) {
+        if (codigo == null) {
+            throw new IllegalArgumentException("codigo não deve ser null!");
+        }
+        this.CNPJOuCPF = codigo;
     }
 
     public String getCNPJOuCPF() {
---
+++ src/test/java/br/ufes/model/ClienteTest.java
@@ -0,0 +1,43 @@
+package br.ufes.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClienteTest {
+
+    public ClienteTest() {
+    }
+
+    /**
+     * Verifica que o codigo de um cliente não pode ser null
+     */
+    @Test
+    public void CT001() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Cliente cliente = new Cliente("Nome", null);
+        });
+
+        String expectedMessage = "codigo não deve ser null!";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+
+    }
+
+    /**
+     * Verifica que o nome de um cliente não pode ser null
+     */
+    @Test
+    public void CT002() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Cliente cliente = new Cliente(null, "12345678901");
+        });
+
+        String expectedMessage = "nome não deve ser null!";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+
+    }
+}
